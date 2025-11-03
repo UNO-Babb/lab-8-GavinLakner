c84e022b00827b82adc8bd4af27888c2e4f40ad0
GavinLakner <gavinlakner@icloud.com>
2025-11-03 05:50:59 +0000

    Procrasination is key

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Gavin Lakner
+#Date: 11/2/2025
+#Assignment: Process Data
 
 import random
 
@@ -11,13 +11,39 @@ def main():
   inFile = open("names.dat", 'r')
   outFile = open("StudentList.csv", 'w')
 
-  #Process each line of the input file and output to the CSV file
 
+#Process each line of the input file and output to the CSV file
 
+#line = inFile.readline()
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    idNum = data[3]
+
+  student_id = makeID(first, last, idNum)
+  output = last + "," + first + "," + student_id + "\n"
+  outFile.write(output)
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+
+def makeID(first, last, idNum):
+  #print(first, last, idNum)
+  idLen = len(idNum)
+  
+  while len(last) < 5:
+    last = last + "X"
+
+  id = first[0] + last + idNum[idLen - 3: ]
+  #don't have time to code the rest which is on me for waiting till 
+  #the last minute, but I understand how to through using already 
+  #written code above to take the first three letters of the major
+  #like in the IDNum and then adding a - before just imputing the
+  #year
+  #print(id)
+  return id
 
 if __name__ == '__main__':
   main()
